f98b1574ce95d31a38c9498534933d2ec1ddefad
RehamReda216 <rehamreda216@gmail.com>
2025-07-08 16:33:44 +0300

    add deisgn colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
     "./src/**/*.{html,js,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+        colors: {
+        'primary-50': '#EFF6F2',
+        'primary-500': '#64967D',
+        'primary-600': '#4A8266',
+        'primary-700': '#37624D',
+        'primary-800': '#254133',
+        'secondary-50': '#F8F6ED',
+        'secondary-400': '#C4B76E',
+      },
+    },
   },
   plugins: [
     require("flowbite/plugin"),
