3dc8c1f1835705f68d0a4b8e28a19dba5cfd15b8
RehamReda216 <rehamreda216@gmail.com>
2025-07-08 17:46:16 +0300

    Edit colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,18 +7,40 @@ module.exports = {
   theme: {
     extend: {
         colors: {
-        'primary-50': '#EFF6F2',
-        'primary-500': '#64967D',
-        'primary-600': '#4A8266',
-        'primary-700': '#37624D',
-        'primary-800': '#254133',
-        'secondary-50': '#F8F6ED',
-        'secondary-400': '#C4B76E',
+        'main': '#64967D', // = primary/500
+        'main-light': '#EFF6F2', // = primary/50
+        'main-dark': '#254133',  // = primary/800
+        primary: {
+          50: '#EFF6F2',
+          100: '#DEede6',
+          200: '#BEDACC',
+          300: '#9DC8B3',
+          400: '#7DB599',
+          500: '#64967D',
+          600: '#4A8266',
+          700: '#37624D',
+          800: '#254133',
+          900: '#12211A',
+          950: '#09100D',
+        },
+        secondary: {
+          50: '#F8F6ED',
+          100: '#F0EDDB',
+          200: '#E2DBB6',
+          300: '#D3C992',
+          400: '#C4B76E',
+          500: '#CFC589',
+          600: '#91843B',
+          700: '#6D632C',
+          800: '#49421D',
+          900: '#24210F',
+          950: '#121107',
+        }
       },
       spacing: {
-      's': '20px',    // Extra Small spacing
+      's': '20px',    // Small spacing
       'md': '60px',    // Medium spacing
-      'l': '100px',   // Extra Large spacing
+      'l': '100px',   // Large spacing
       },
       fontFamily: {
         tajawal: 'Tajawal',
