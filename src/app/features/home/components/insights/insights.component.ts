c4ca10fad56cb97451e3beb14e9cfa843185db7c
RehamReda216 <rehamreda216@gmail.com>
2025-07-19 02:31:03 +0300

    change it into icons

--- src/app/features/home/components/insights/insights.component.ts
+++ src/app/features/home/components/insights/insights.component.ts
@@ -7,24 +7,42 @@ import { Component } from '@angular/core';
   styleUrl: './insights.component.css'
 })
 export class InsightsComponent {
+  // cards = [
+  //   {
+  //     image: 'assets/insights_section/Dove.png',
+  //     value: 89,
+  //     label: 'ملائكة التوصيل',
+  //     alt: 'Delivery angels icon'
+  //   },
+  //   {
+  //     image: 'assets/insights_section/Online_Shop.png',
+  //     value: 45,
+  //     label: 'الشركاء المباركون',
+  //     alt: 'Partner stores icon'
+  //   },
+  //   {
+  //     image: 'assets/insights_section/Password.png',
+  //     value: 1234 ,
+  //     label: 'الوجبات المشاركة',
+  //     alt: 'Shared meals icon'
+  //   }
+  // ];
   cards = [
-    {
-      image: 'assets/insights_section/Dove.png',
-      value: 89,
-      label: 'ملائكة التوصيل',
-      alt: 'Delivery angels icon'
-    },
-    {
-      image: 'assets/insights_section/Online_Shop.png',
-      value: 45,
-      label: 'الشركاء المباركون',
-      alt: 'Partner stores icon'
-    },
-    {
-      image: 'assets/insights_section/Password.png',
-      value: 1234 ,
-      label: 'الوجبات المشاركة',
-      alt: 'Shared meals icon'
-    }
-  ];
+  {
+    icon: 'fa-dove',
+    value: 89,
+    label: 'ملائكة التوصيل',
+  },
+  {
+    icon: 'fa-store',
+    value: 45,
+    label: 'الشركاء المباركون',
+  },
+  {
+    icon: 'fa-bag-shopping',
+    value: 1234,
+    label: 'الوجبات المشاركة',
+  }
+];
+
 }
