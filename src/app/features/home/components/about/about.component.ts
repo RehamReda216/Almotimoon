7588f6c95c925f024f3c70b2f3b02e274e711edd
RehamReda216 <rehamreda216@gmail.com>
2025-07-19 01:07:19 +0300

    create about section

--- src/app/features/home/components/about/about.component.ts
+++ src/app/features/home/components/about/about.component.ts
@@ -7,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
-
+  sectionData=[
+    {
+      id:1,
+      headline: "فضل الإطعام",
+      subline: "في كل كبدٍ رطبةٍ أجر",
+      text_section1: "انطلاقاً من حديث",
+      highlighted_text: "'خيركم من أطعم الطعام'،",
+      text_section2: " يربط المطعمون المطاعم والمطابخ المنزلية والمتطوعين بجيراننا المحتاجين. كل وجبة يتم تحضيرها بنفس الجودة والحب الذي تقدمه لعائلتك.",
+      image:"/assets/about_section/about_section_img1.png",
+      imageAlt:"volunteer"
+    },
+    {
+      id:2,
+      headline: "المطْعِمُون",
+      subline: "أكثر من وجبة - جسر من الكرامة",
+      text_section1: "انطلاقاً من حديث",
+      highlighted_text: "'خيركم من أطعم الطعام'،",
+      text_section2: " يربط المطعمون المطاعم والمطابخ المنزلية والمتطوعين بجيراننا المحتاجين. كل وجبة يتم تحضيرها بنفس الجودة والحب الذي تقدمه لعائلتك.",
+      image:"/assets/about_section/about_section_img2.png",
+      imageAlt:"volunteering"
+    }
+  ]
 }
