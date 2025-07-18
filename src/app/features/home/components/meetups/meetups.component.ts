5f963768d53081e16639771fd4c21d8e0995ad8b
Abdallah Mohamed Gomaa <abdallah.gomaa921@gmail.com>
2025-07-18 21:01:48 +0300

    Add reusable section title component

--- src/app/features/home/components/meetups/meetups.component.ts
+++ src/app/features/home/components/meetups/meetups.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { SectionComponent } from "../../../../shared/components/section/section.component";
 
 @Component({
   selector: 'app-meetups',
-  imports: [],
+  imports: [SectionComponent],
   templateUrl: './meetups.component.html',
   styleUrl: './meetups.component.css'
 })
