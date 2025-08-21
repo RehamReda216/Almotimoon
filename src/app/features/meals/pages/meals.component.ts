cd577f69ae09862850a6d7f25230986d7d247fd3
RehamReda216 <rehamreda216@gmail.com>
2025-08-21 13:50:40 +0300

    create meal page

--- src/app/features/meals/pages/meals.component.ts
+++ src/app/features/meals/pages/meals.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Carousel } from 'flowbite';
 
 @Component({
   selector: 'app-meals-page',
@@ -7,5 +9,33 @@ import { Component } from '@angular/core';
   styleUrl: './meals.component.css'
 })
 export class MealsPageComponent {
+  constructor( private router: Router) {}
+  goToRegistration(){
+        this.router.navigate(['/registration']);
+  }
+  infoData=[
+    {
+      id:1,
+      headline: "أماكن التوزيع",
+      img_url:"assets/meals-page/location-img.png",
+      sentences: [
+        " الاسكندرية- المنشية",
+        "الاسكندرية- سيدي جابر",
+        "المعادي",
+        "قرى الجيزة"
+      ]
+    },
+    {
+      id:2,
+      headline: "أثر تبرعك",
+      img_url:"assets/meals-page/volunteer-img.png",
+      sentences: [
+        " 4 وجبات تُغني أُسرة عن الجوع",
+        "وجبة ساخنة تساعد طفل عالحياة",
+        "سقي ماء يحافظ على كرامة إنسان",
+        "اطعام أطفال يبنوا مستقبل غد"
+      ]
+    }
+  ];
 
 }
