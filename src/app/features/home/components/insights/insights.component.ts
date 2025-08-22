e2b81441ce5eff1338f4a14bc2e513bcce6f13e3
RehamReda216 <rehamreda216@gmail.com>
2025-08-22 18:54:02 +0300

    add button functionality

--- src/app/features/home/components/insights/insights.component.ts
+++ src/app/features/home/components/insights/insights.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { SectionComponent } from '../../../../shared/components/section/section.component';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-insights',
@@ -8,38 +10,45 @@ import { SectionComponent } from '../../../../shared/components/section/section.
   styleUrl: './insights.component.css'
 })
 export class InsightsComponent {
+  constructor(private router: Router) {}
+  
   cards = [
-  {
-    icon: 'fa-dove',
-    value: 89,
-    label: 'ملائكة التوصيل',
-  },
-  {
-    icon: 'fa-store',
-    value: 45,
-    label: 'الشركاء المباركون',
-  },
-  {
-    icon: 'fa-bag-shopping',
-    value: 1234,
-    label: 'الوجبات المشاركة',
-  }
+    {
+      icon: 'fa-dove',
+      value: 89,
+      label: 'ملائكة التوصيل',
+    },
+    {
+      icon: 'fa-store',
+      value: 45,
+      label: 'الشركاء المباركون',
+    },
+    {
+      icon: 'fa-bag-shopping',
+      value: 1234,
+      label: 'الوجبات المشاركة',
+    }
   ];
+
   blogCards = [
-  {
-    id: 1,
-    title: '5000 وجبة... في يوم واحد!',
-    text: 'جهود مذهلة من مطبخ الخير وشركائنا في أحد أكبر حملاتنا.',
-    image: '/assets/insights_section/blog_card1.png',
-    alt: 'volunteer'
-  },
-  {
-    id: 2,
-    title: 'خلف كل وجبة.. متطوع لا يُرى',
-    text: 'نروي لكم حكاية أحمد الذي يقطع يوميًا 30 كم لتوصيل الطعام للمحتاجين',
-    image: '/assets/insights_section/blog_card2.png',
-    alt: 'volunteer'
+    {
+      id: 1,
+      title: '5000 وجبة... في يوم واحد!',
+      text: 'جهود مذهلة من مطبخ الخير وشركائنا في أحد أكبر حملاتنا.',
+      image: '/assets/insights_section/blog_card1.png',
+      alt: 'volunteer'
+    },
+    {
+      id: 2,
+      title: 'خلف كل وجبة.. متطوع لا يُرى',
+      text: 'نروي لكم حكاية أحمد الذي يقطع يوميًا 30 كم لتوصيل الطعام للمحتاجين',
+      image: '/assets/insights_section/blog_card2.png',
+      alt: 'volunteer'
+    }
+  ];
+
+  goToBlogs(){
+      this.router.navigate(['/blogs']);
   }
-];
 
 }
