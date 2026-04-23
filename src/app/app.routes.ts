75f0656af6840fe9e21dc99f9e7434d5fe1d0199
hager445 <ho2333535@gmail.com>
2026-04-23 13:42:18 +0200

    adding dashboard app folder

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,37 +1,16 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './features/home/pages/home.component';
-import { MeetupsPageComponent } from './features/meetups/pages/meetups.component';
-import { MealsPageComponent } from './features/meals/pages/meals.component';
-
-import { ProfileComponent } from './features/profile/pages/profile.component';
 
 export const routes: Routes = [
-
-    // {
-    //     path:'',
-    // loadChildren: () =>
-    // import('./features/auth/auth.routes').then(m => m.routes)
-    // },
-   { path: '', redirectTo: '/home', pathMatch: 'full' },
-   { path: 'home', component: HomeComponent },
-    {
-        path:'meetups',
-        component:MeetupsPageComponent,
-        title: 'Meetups'
-    },
-    {
-        path:'meals',
-        component:MealsPageComponent,
-        title: 'Meals'
-    },
-
-    {
-        path:'profile',
-        component:ProfileComponent,
-        title: 'Profile'
-    },
-    {
-        path:'**',
-        redirectTo:''
-    },
+  //   {
+  //     path: '',
+  //     loadChildren: () =>
+  //       import('./user-app/features/auth/auth.routes').then((m) => m.routes),
+  //   },
+  {
+    path: '',
+    loadChildren: () =>
+      import('./user-app/features/user-layout/user-layout-routes').then(
+        (m) => m.routes,
+      ),
+  },
 ];
