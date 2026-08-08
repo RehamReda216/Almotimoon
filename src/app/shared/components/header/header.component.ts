35528530da14ace3cdad78890cd66758e4fe9b75
RehamReda216 <rehamreda216@gmail.com>
2026-08-08 14:57:42 +0300

    change header location into public shared

---
+++ src/app/shared/components/header/header.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { ButtonComponent } from '../../../user-app/shared/components/button/button.component';
+
+@Component({
+  selector: 'app-header',
+  imports: [RouterLink,CommonModule,ButtonComponent,RouterLinkActive],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+  isOpened:boolean=false;
+applyNavToggle(){
+this.isOpened = !this.isOpened;
+}
+}
