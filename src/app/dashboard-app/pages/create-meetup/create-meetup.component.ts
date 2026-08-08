5f1fe6a2e482a4380e8734d14b0372cdbd1df85c
RehamReda216 <rehamreda216@gmail.com>
2026-08-08 14:55:49 +0300

    add create meetup component

---
+++ src/app/dashboard-app/pages/create-meetup/create-meetup.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-meetup',
+  imports: [],
+  templateUrl: './create-meetup.component.html',
+  styleUrl: './create-meetup.component.css'
+})
+export class CreateMeetupComponent {
+
+}
