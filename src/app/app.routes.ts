5cc1d0302983aa6f1200254f21045d098c2b2826
RehamReda216 <rehamreda216@gmail.com>
2026-08-08 14:54:47 +0300

    make main layout for user app and dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,26 @@
 import { Routes } from '@angular/router';
-
-import { HomeComponent } from './user-app/features/home/pages/home.component';
-import { MeetupsPageComponent } from './user-app/features/meetups/pages/meetups.component';
-import { MealsPageComponent } from './user-app/features/meals/pages/meals.component';
-import { ProfileComponent } from './user-app/features/profile/pages/profile.component';
-import { BlogsComponent } from './user-app/features/meetups/components/blogs/blogs.component';
-import { LoginComponent } from './user-app/features/auth/components/login/login.component';
+import { MainLayoutComponent } from './shared/layouts/main-layout/main-layout.component';
 
 export const routes: Routes = [
   {
     path: '',
-    loadChildren: () =>
-      import('../app/user-app/features/user-layout/user-layout-routes').then(
-        (m) => m.routes,
-      ),
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () =>
+          import('./user-app/users.routes').then(
+            (m) => m.USER_ROUTES
+          )
+      },
+      {
+            path: 'dashboard',
+            loadChildren: () =>
+              import('./dashboard-app/dashboard.routes').then(
+                (m) => m.DASHBOARD_ROUTES
+              )
+      },
+    ]
   },
   {
     path: 'auth',
