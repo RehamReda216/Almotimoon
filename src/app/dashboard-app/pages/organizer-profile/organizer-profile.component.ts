368d8f7b9cd159742f0010b1b5e08b768db00c01
RehamReda216 <rehamreda216@gmail.com>
2026-08-08 14:56:17 +0300

    add organizer profile component

---
+++ src/app/dashboard-app/pages/organizer-profile/organizer-profile.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-organizer-profile',
+  imports: [],
+  templateUrl: './organizer-profile.component.html',
+  styleUrl: './organizer-profile.component.css'
+})
+export class OrganizerProfileComponent {
+
+}
